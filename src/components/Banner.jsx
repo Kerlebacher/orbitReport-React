ae3dd774de361bc75867c3abb347604cce251db6
Kerlebacher <154646865+Kerlebacher@users.noreply.github.com>
2024-03-21 21:29:35 +0000

    banner complete

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./styling.css";
+
+function Banner() {
+    return (
+    <>
+    <header>Orbit Report</header>
+    <p>
+Click on the buttons to see the satellites in that orbit type
+    </p>
+    </>
+    );
+}
+
+export default Banner;
